6b5e115036ee8cd7f787c342a750b2cd9b0dcedb
Xforzan <nakhila2024@gmail.com>
2025-10-23 22:31:26 +0200

    Loading component

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,11 @@
+import "./Loading.css";
+
+const Loading = () => {
+  return (
+    <div className="spinner-container">
+      <div className="spinner"></div>
+    </div>
+  );
+};
+
+export default Loading;
